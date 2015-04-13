30cd8a84a2a9e4f51f65f84e2ca8423636c964f4
Tom Hulihan <hulihan.tom159@gmail.com>
2015-04-13 12:38:05 -0400

    Only use one password hash and salt during tests
    This speeds up the tests ~3x.

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
   factory :user, class: Tabular::Models::User do
     sequence(:username) { |n| "test_user_#{n}" }
-    password_salt { Tabular::Services::Passwords.generate_salt }
-    password_hash do
-      Tabular::Services::Passwords.hash_password('password', password_salt)
-    end
+
+    # For efficiency, use the same password salt and hash in every test.
+    salt = Tabular::Services::Passwords.generate_salt
+    hash = Tabular::Services::Passwords.hash_password('password', salt)
+    password_salt salt
+    password_hash hash
   end
 end
