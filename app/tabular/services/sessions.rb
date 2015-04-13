556f58d979874b605fdd426e341633ce00690178
Tom Hulihan <hulihan.tom159@gmail.com>
2015-04-13 13:37:46 -0400

    Make the sessions service more rigorous

--- app/tabular/services/sessions.rb
+++ app/tabular/services/sessions.rb
@@ -5,7 +5,9 @@ module Tabular
       module_function
 
       # If the given credentials are valid, create a session.
-      def login!(user, password)
+      def login!(username, password)
+        user = Models::User.find_by(username: username)
+        fail Errors::NoSuchModel unless user
         id, salt, hash = user.id, user.password_salt, user.password_hash
         Passwords.authenticate!(password, salt, hash)
         Models::Session.create!(key: unique_session_keys.next, user_id: id)
@@ -13,6 +15,7 @@ module Tabular
 
       # Destroy all the sessions with the given key.
       def logout!(key)
+        fail Errors::Unauthorized unless Models::Session.exists?(key: key)
         Models::Session.destroy_all(key: key)
       end
 
--- spec/app/tabular/services/sessions_spec.rb
+++ spec/app/tabular/services/sessions_spec.rb
@@ -1,24 +1,58 @@
 require 'spec_helper'
 
 describe Tabular::Services::Sessions do
-  let(:user) { create(:user) }
-
   describe '#login!' do
-    it 'creates a new session for the user' do
-      expect(subject.login!(user, 'password')).to be_a(Tabular::Models::Session)
+    let(:user) { create(:user) }
+    let(:username) { user.username }
+    let(:password) { 'password' }
+
+    context 'when the user does not exist' do
+      let(:username) { 'typod username' }
+
+      it 'fails with NoSuchModel' do
+        expect { subject.login!(username, password) }
+          .to raise_error(Tabular::Errors::NoSuchModel)
+      end
+    end
+
+    context 'when the user exists' do
+      context 'but the password is wrong' do
+        let(:password) { 'letmein' }
+
+        it 'fails with BadPassword' do
+          expect { subject.login!(username, password) }
+            .to raise_error(Tabular::Errors::BadPassword)
+        end
+      end
+
+      context 'and the password is correct' do
+        it 'creates a new session for the user' do
+          expect(subject.login!(user.username, 'password'))
+            .to be_a(Tabular::Models::Session)
+        end
+      end
     end
   end
 
   describe '#logout!' do
-    let(:session) { build(:session, user: user) }
-
-    before do
-      session.save!
-      subject.logout!(session.key)
+    context 'when the session does not exist' do
+      it 'fails with Unauthorized' do
+        expect { subject.logout!('fake session') }
+          .to raise_error(Tabular::Errors::Unauthorized)
+      end
     end
 
-    it 'destroys all of the sessions for the user' do
-      expect(Tabular::Models::Session.where(id: session.id)).to be_empty
+    context 'when the session exists' do
+      let(:session) { build(:session) }
+
+      before { session.save! }
+
+      it 'destroys that session' do
+        expect { subject.logout!(session.key) }
+          .to change { Tabular::Models::Session.where(id: session.id).count }
+          .from(1)
+          .to(0)
+      end
     end
   end
 end
