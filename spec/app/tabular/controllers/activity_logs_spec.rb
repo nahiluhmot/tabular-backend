b9c9f20d153f0efe20d995bc623930d438bb97d6
Tom Hulihan <hulihan.tom159@gmail.com>
2015-04-18 12:56:02 -0400

    remove temporary testing tag

--- spec/app/tabular/controllers/activity_logs_spec.rb
+++ spec/app/tabular/controllers/activity_logs_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Tabular::Controllers::ActivityLogs, :cur do
+describe Tabular::Controllers::ActivityLogs do
   subject { last_response }
   let(:app) { described_class.new }
   let(:status) { subject.status }
